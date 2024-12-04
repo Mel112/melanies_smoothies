8de03a28cf2357acce87de275e2f558f0fdcce30
Melchizedek Recalde <70351909+Mel112@users.noreply.github.com>
2024-12-04 13:41:52 +0800

    Update streamlit_app.py
    Put the request of the nutrition information to another line

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,10 +2,6 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 
-import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json())
-
 # Write directly to the app
 st.title("Customize Your Smoothie :balloon:")
 st.write(
@@ -41,6 +37,10 @@ if ingredients_list:
     #st.write(my_insert_stmt)
 
     time_to_insert = st.button('Submit Order')
+
+    import requests
+    smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+    st.text(smoothiefroot_response.json())
 
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
